38d8884110ee27e4b1559a20881a596fe5e7a835
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-10-03 14:06:44 -0300

    initial project

---
+++ ProjectSimple/ProjectSimple.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ProjectSimple.Droid
+{
+    [Activity(Label = "ProjectSimple", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.MainActivity);
+
+
+        }
+    }
+}
+
---
+++ ProjectSimple/ProjectSimple/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSimple
+{
+    class MyClass
+    {
+        public static int Soma(int num, int num2)
+        {
+            return num + num2;
+        }
+    }
+}
